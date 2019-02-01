1afad865e241d761e8a849d2421a7842002cf869
王永山 <lygwys@outlook.com>
2019-02-01 13:37:54 +0800

    带病提交31个变更

---
+++ src/booklist-aspnet-core/src/lygwys.BookList.Application/BookListManagement/BookTags/Dtos/GetBookTagForEditOutput.cs
@@ -0,0 +1,15 @@
+
+
+using System.Collections.Generic;
+using Abp.Application.Services.Dto;
+using lygwys.BookList.BookListManagement.BookTags;
+
+namespace lygwys.BookList.BookListManagement.BookTags.Dtos
+{
+    public class GetBookTagForEditOutput
+    {
+
+        public BookTagEditDto BookTag { get; set; }
+
+    }
+}
